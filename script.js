fa998765887b3002bee24b7435d7a6e85957266a
ShaikImambe <shaikimambe24@gmail.com>
2025-11-28 16:56:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.getElementById("shopBtn").addEventListener("click", () => {
+window.location.href = "https://www.flipkart.com";
+});
+
+
+
+document.getElementById("readMore").addEventListener("click", () => {
+alert("More details will be available soon!");
+});
